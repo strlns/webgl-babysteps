0a86e0e8e59ad3f6f7d22270cb89a6ca2298b51f
Moritz Rehbach <moritzrehbach@gmail.com>
2021-06-13 10:23:08 +0200

    Add AmbientLight

---
+++ js/light/addDirectionalLight.js
@@ -0,0 +1,23 @@
+import {DirectionalLight, DirectionalLightHelper} from "../lib/three.module.js";
+
+
+export default function addDirectionalLight(
+    scene,
+    color = [0xffafff],
+    intensity = 5,
+    position = [10, 5, 0],
+    targetPosition = [2.5, 1, 1]
+) {
+    const light = new DirectionalLight(color, intensity);
+    light.position.set(...position);
+    light.target.position.set(...targetPosition);
+    const lightHelper = new DirectionalLightHelper(light);
+    scene.add(lightHelper);
+    scene.add(light)
+    scene.add(light.target)
+    const updateLight = () => {
+        light.target.updateMatrixWorld();
+        lightHelper.update()
+    }
+    updateLight()
+}
--- js/main.mjs
+++ js/main.mjs
@@ -2,32 +2,28 @@ import throwIfNoWebGl from "./webgl-availability/throwIfNoWebGl.mjs";
 import initScene from "./scene/scene.mjs";
 import {cube} from "./polyeder/polyeder.mjs";
 import Renderer from "./scene/renderer.mjs";
-import {DirectionalLight, DirectionalLightHelper} from "./lib/three.module.js";
+import addDirectionalLight from "./light/addDirectionalLight.js";
+import addAmbientLight from "./light/addAmbientLight.js";
 
 throwIfNoWebGl();
 document.addEventListener('DOMContentLoaded', () => {
     const {scene, camera} = initScene();
     scene.add(cube);
     const renderer = new Renderer(camera, scene);
-    const light = new DirectionalLight(0xffafff, 5);
-    light.position.set(10, 5, 0);
-    light.target.position.set(2.5, 1, 1);
-    const lightHelper = new DirectionalLightHelper(light);
-    scene.add(lightHelper);
-    scene.add(light)
-    scene.add(light.target)
-    const updateLight = () => {
-        light.target.updateMatrixWorld();
-        lightHelper.update()
-    }
-    updateLight()
+    addAmbientLight(scene)
+    addDirectionalLight(scene)
     camera.updateProjectionMatrix()
     renderer.startRendering(
         (frame) => {
-            cube.rotation.x += 0.01
-            cube.rotation.y += 0.01
             if (frame % 64 > 32) {
                 cube.rotation.y -= 0.1
+            } else {
+                cube.rotation.y += 0.01
+            }
+            if (frame % 256 > 128) {
+                cube.rotation.x -= 0.1
+            } else {
+                cube.rotation.x += 0.01
             }
         }
     );
